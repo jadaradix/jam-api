e73ffc00e866c434d22a73867c4c51da2ccdbfb4
James Garner <j@jada.io>
2016-10-16 18:20:53 +0100

    Complete API.

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 
 const restify = require("restify");
 const server = restify.createServer();
-
 const port = process.env.JAM_API_PORT || 8080;
 
-server.get("/hello/:name", (req, res, next) => {
-  res.send({
-    "name": req.params.name
-  });
+const products = require("./lib/products");
+
+server.get("/products", (req, res, next) => {
+  res.send(products);
   return next();
+});
+
+server.get("/products/:id", (req, res, next) => {
+  const product = products.find((product) => (product.id === req.params.id));
+  if (product) {
+    res.send(product);
+    return next();
+  } else {
+    return next(new restify.NotFoundError("Product not found"));
+  }
 });
 
 server.listen(port, () => {
